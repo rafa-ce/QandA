db32928129db657f39a1f03a90c89f683593362f
Rafa C <rafasantcont@yahoo.com.br>
2021-08-01 20:00:51 -0300

    manipulando evento de clique de botão e entrada no input

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { UserIcon } from './Icons';
 
-export const Header = () => (
-  <div>
-    <a href="./">Q & A</a>
-    <input type="text" placeholder="Search..." />
-    <a href="./signin">
-      <UserIcon />
-      <span>SignIn</span>
-    </a>
-  </div>
-);
+export const Header = () => {
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <a href="./">Q & A</a>
+      <input type="text" placeholder="Search..." onChange={handleSearchInputChange} />
+      <a href="./signin">
+        <UserIcon />
+        <span>SignIn</span>
+      </a>
+    </div>
+  );
+};
